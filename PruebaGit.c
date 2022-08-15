779c9c989a576ad0ed43936afb21df7adfa818ca
JoseAntonioLP01 <joseanto29lp@gmail.com>
2022-08-15 18:16:03 -0500

    Prueba de codigo

---
+++ PruebaGit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("Hola mundo\n");
+	printf("Numero 1: ");
+	scanf("%d", &a);
+	printf("Numero 2: ");
+	scanf("%d", &b);
+	c=a+b;
+	printf("Resultado: %d\n", c);
+	return 0;
+}
