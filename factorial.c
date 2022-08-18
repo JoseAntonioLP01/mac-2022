6559d63567734f68907f2ae1558bb9d8bb9018e3
JoseAntonioLP01 <joseanto29lp@gmail.com>
2022-08-18 07:48:49 -0500

    Haciendo prueba de subida de carpetas

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int numero, contador, factorial;
+	
+	factorial=1;
+    printf("Ingresa el numero que deseas obtener el factorial: ");
+    scanf("%d", &numero);
+
+    for (contador=1; contador<=numero; contador++)
+    {
+		factorial=factorial*contador;
+    }
+
+	printf("El factorial del numero escrito es: %d\n", factorial);
+	
+    return 0;
+}
